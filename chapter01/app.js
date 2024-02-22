e2e778f0d9ecc474e8c24b39ca825872bf09ad6d
Talha Arshad <155132827+Talha-Arshad8@users.noreply.github.com>
2024-02-22 22:37:29 +0500

    chap1 and chap2 done

---
+++ chapter01/app.js
@@ -0,0 +1,8 @@
+
+
+//q1 dislay showing
+
+alert("Welcome to JS Land...Happy Coding!")
+alert("Welcome to JS Land")
+alert("Happy Codding!")
+alert("Hello... I can run JS through my web browser's console")
---
+++ chapter02/app.js
@@ -0,0 +1,31 @@
+
+
+//q1 variable name
+
+var UserName = "Talha"
+alert(UserName)
+
+//q2 fullname
+
+var FullName = "Talha Arshad"
+alert(FullName)
+
+//q3 hello worls showing
+
+
+var Message = "Hello World"
+alert(Message)
+
+//q4 variable email
+
+var email = "talhaattari04@gmail.com"
+alert(email)
+
+
+//q5 variable book
+
+var book = "A smarter way to learn JavaScript"
+alert(book)
+
+
+
