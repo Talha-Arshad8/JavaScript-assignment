0623a5e84131838ec5c6567c32c1fa59538bcc33
Talha Arshad <155132827+Talha-Arshad8@users.noreply.github.com>
2024-02-29 14:01:56 +0500

    arithmatic work add

---
+++ Arithmatic assighnment/app.js
@@ -0,0 +1,36 @@
+
+//q1
+var x = 25
+var y = 7
+var modula = x % y
+console.log(modula)
+
+//q3
+var c = 12
+var d = 3
+var exponantiation = c ** d
+console.log(exponantiation)
+
+//q5
+
+var g = 20
+var h = 4
+var divide = g / h 
+//after 5 add in g
+var add = g + 5 / h
+console.log(divide)
+console.log(add)
+
+//q6
+var i =  8
+var j = 3
+var multiply = i * j
+var subtract = i - 2 * j
+console.log(multiply)
+console.log(subtract)
+
+//q7
+var k = 18
+var l = 5
+var subtract  = k - l + 2
+console.log(subtract )
---
+++ chapter03/app.js
@@ -0,0 +1,34 @@
+
+
+//q1 variable name
+
+var UserName = "Talha"
+alert(UserName)
+
+//q2 fullname
+
+var FullName = "Talha Arshad"
+alert(FullName)
+
+//q3 hello worls showing
+
+
+var Message = "Hello World"
+alert(Message)
+
+//q4 variable email
+
+var email = "talhaattari04@gmail.com"
+alert(email)
+
+
+//q5 variable book
+
+var book = "A smarter way to learn JavaScript"
+alert(book)
+
+//q6 alert one js
+
+alert("PIZZA \nPIZZ \nPIZ \nPI \nP")
+
+
