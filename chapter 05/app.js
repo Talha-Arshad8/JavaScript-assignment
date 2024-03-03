06bbf7d97cb0f7539b55d0484db727bbb438ed13
Talha Arshad <155132827+Talha-Arshad8@users.noreply.github.com>
2024-03-03 15:26:50 +0500

    increment and decrement done

---
+++ chapter 05/app.js
@@ -0,0 +1,59 @@
+
+//q1
+var count = 8
+var pcount = count++
+console.log(count)
+//q2
+
+var x = 15
+var decrement  = --x
+console.log(x)
+
+//q3
+
+var num = 25
+var decrementNum = num--
+console.log(num)
+
+//q4
+
+var value = 10
+var newValue = ++value
+console.log(value)
+
+//q5
+
+var index = 5
+var newindex = index++
+console.log(index)
+
+//q6
+
+var quantity = 12
+var quantit = --quantity
+console.log(quantity)
+
+//q7
+
+var counter = 18
+var counte = counter++
+console.log(counter)
+
+//q8
+
+var score = 30
+var scor = score--
+console.log(score)
+
+//q9
+
+var value = 20
+var newvalue = ++value
+console.log(value)
+console.log(newvalue)
+
+//q10
+
+var counter = 8
+var counter = counter--
+console.log(counter)
