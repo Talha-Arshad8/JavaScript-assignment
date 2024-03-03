94c4c7d8d8eeef69129b7474ef9b5169f6f08020
Talha Arshad <155132827+Talha-Arshad8@users.noreply.github.com>
2024-03-03 16:05:56 +0500

    chapter 4 add

--- chapter04/app.js
+++ chapter04/app.js
@@ -1,5 +1,17 @@
-
+var name = "Talha"
 
 //q1
+//a
+document.write('<h1>Rules for naming JS variables</h1>')
 
-//var,let,constanat
+
+
+//b
+document.write('<p>Variables name can only contain, number, $ and _. For example : $Talha</p>')
+//c
+document.write('Variable must be begin with a letter, $ or _ For example : $name _ name or name')
+//d
+document.write('<p>Variables name are case sensitive</p>')
+//e
+document.write('<p>Variables names should not be JS keyword</p>')
+
