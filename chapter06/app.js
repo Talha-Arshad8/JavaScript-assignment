005d24438709365b2afd7f2e73fe7581201ddab2
Talha Arshad <155132827+Talha-Arshad8@users.noreply.github.com>
2024-03-05 12:35:20 +0500

    css edit

---
+++ chapter06/app.js
@@ -0,0 +1,24 @@
+//q1
+// var num = prompt('Enter a number and check  it is even or odd')
+// if(num % 2 == 0 ) {
+//     document.write(`<h2>${num} is even </h2>`)
+// } else {
+//     document.write(`<h2>${num} is odd </h2>`)
+// }
+//q13
+
+// var number = prompt ('Enter a number and check if it vowel or consonanat')
+// if(number == a,e,i,o,u){
+//     document.write(`<h2>${number}This is a vowel</h2>`)
+// }
+// else{
+//     document.write(`This is a consonanat`)
+// }
+
+//q11
+
+var temperature = prompt('Enter a temperature celcius and covert it fahrenheit')
+if(temperature * 9/5 + 32){
+    document.write(`${temperature} F`)
+}
+
