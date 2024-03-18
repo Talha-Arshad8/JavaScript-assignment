d2ae2e51b00aee80359bfbf34278dbfd2f0dbda5
Talha Arshad <155132827+Talha-Arshad8@users.noreply.github.com>
2024-03-18 12:23:14 +0500

    array add

---
+++ chapter07/app.js
@@ -0,0 +1,55 @@
+//q1
+var future = ["Ayan" , "Haris" , "Huzaifa" , "Asghar" , "Azfer" , "Imran" , "Taha"]
+future.push("Talha")
+console.log(future)
+
+
+//q2
+var qualification = ["BSC" , "SSC" , "HSC" , "BS" , "BCOM" , "MS" , "Phd"]
+console.log(qualification)
+
+//q3
+
+var courses = ["Web developer" , "Graphic design" , "cctv installation" , "laptop repiaring" , "mobile repiaring" , "solar system" , "python" , "flutter" , "bike repiaring" , "Ac repiaring"]
+console.log(courses)
+console.log(courses.length)
+
+//q4
+
+var car = ["Audi" , "lamborgini" , "Bmw" , "Supra"]
+console.log ( car[0])
+console.log(car[3])
+
+
+
+//q5
+
+var colour = ["Red" , "Yellow" , "Green" , "purple" , "Orange" , "Blue"]
+console.log(colour);
+//a
+// var startcolour = prompt("Enter a colour and add begining")
+// colour.unshift(startcolour)
+//b
+// var endcolour = prompt("Enter a colour and add end of the array")
+// colour.push(endcolour)
+//c
+// console.log("after add two colour --> colour");
+// colour.unshift("voilet" , "deep red")
+// console.log(colour);
+//d
+// console.log("after delete fisrt of the array");
+// colour.shift()
+// console.log(colour);
+//e
+// console.log("after delete end of the arrey -->colour");
+// colour.pop()
+// console.log(colour);
+//f
+var addcolour = +prompt("enter a index num")
+var addname = prompt("Enter a colour name")
+var usercolour = colour.indexOf(addcolour)
+var username = colour.indexOf(addname)
+if(addcolour === 0 || 1 || 2 ||3 || 4 || 5 ||6){
+    
+}
+console.log(colour)
