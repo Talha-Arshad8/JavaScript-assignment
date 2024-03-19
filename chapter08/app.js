79126e9ea43aa23063c317211fde8d4c93c9b313
Talha Arshad <155132827+Talha-Arshad8@users.noreply.github.com>
2024-03-19 22:46:23 +0500

    loop assigment add

---
+++ chapter08/app.js
@@ -0,0 +1,44 @@
+//q1
+// for (t = 1; t <= 10; t++ ){
+//     console.log(t);
+// }
+
+//q2
+
+// for (t = 1; t <= 20; t++) {
+//     if(t % 2 == 0){
+//         console.log(t);
+//     }
+// }
+
+//q3
+
+// for (t = 1; t <= 15; t++) {
+//     if(t % 2 == 1){
+//         console.log(t);
+//     }
+// }
+
+//q4
+
+// for (t = 1; t <= 10; t++) {
+//     console.log(7 * t);
+// }
+
+//q5
+
+// for (t = 1; t <= 10; t++) {
+//     console.log(t * t);
+// }
+
+//q6
+
+// for (t = 1; t = 1; t++) {   
+//     console.log("Hello");
+// }
+
+//q7
+
+for (t = 1; t <= 3; t++) {
+    console.log(t + 1);
+}
