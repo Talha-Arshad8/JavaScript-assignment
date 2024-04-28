77981cdd6577b18e80a6f335226177769768e096
Talha Arshad <155132827+Talha-Arshad8@users.noreply.github.com>
2024-04-28 11:53:53 +0500

    add function assignment

---
+++ funtion-Assignment/app.js
@@ -0,0 +1,26 @@
+//q1 show current date
+var currentdate = new Date();
+document.write(currentdate);
+
+//q2 
+// var firstName = prompt("Enter first Name")
+// var lastName = prompt("Enter last Name")
+// function greet(){
+//     console.log(firstName + " " +  lastName);
+// }
+// greet();
+//q3
+function greet(num1,num2){
+    console.log(num1 + num2);
+}
+greet(4,3);
+greet(3,6);
+
+//q4
+
+function square(num){
+    console.log(num * num);
+}
+square(3);
+
+//q5
