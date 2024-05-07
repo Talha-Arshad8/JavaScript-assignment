92b166753633239b939c338e03f7ac714df046d4
Talha Arshad <155132827+Talha-Arshad8@users.noreply.github.com>
2024-05-07 20:00:03 +0500

    add work

--- soccer-game/app.js
+++ soccer-game/app.js
@@ -1,32 +1,40 @@
-var minute = 5;
-var second = 60;
+var targetNum, p1score, p2score
+var target = document.getElementById("targetNum")
+var winnerContainer = document.getElementById("winner")
+var btn1 = document.getElementById("score-1")
+var btn2 = document.getElementById("score-2")
+var scoreBoardP1 = document.getElementById("p1score")
+var scoreBoardP2 = document.getElementById("p2score")
+var scoreContainer = document.getElementById("score-container")
 
-setInterval(function time(){
-    if(minute == 0 && second == 1){
-        document.getElementById("counter").innerHTML = "00:00"
-    }else{
-        second--;
-        if(second == 0){
-            minute--;
-            second = 60;
-            if(minute == 0){
-                minute = minute;
-            }
-        }
-        document.getElementById("counter").innerHTML = minute + ":" + second;
+function start(){
+    targetNum = +target.value;
+    if (!targetNum && targetNum < 1){
+        alert("Please enter positive num");
+        return;
+        
     }
-},1000)
-function score(){
-    var scoreBoard = document.getElementById("p1score");
-    p1score = Number(scoreBoard.innerText);
-    p1score++
-    console.log(p1score);
-    scoreBoard.innerText = p1score;
 }
-function score2(){
-    var scoreBoard = document.getElementById("p2score");
-    p2score = Number(scoreBoard.innerText);
-    p2score++
-    console.log(p2score);
-    scoreBoard.innerText = p2score;
+function score1(){
+    var winner = document.getElementById("winner")
+    if (target  === "p1score"){
+        p1score = Number(scoreBoardP1.innerText);
+        p1score++;
+        scoreBoardP1.innerText = p1score;
+        if (p1score === targetNum){
+            btn1.setAttribute("disabled", "true")
+            btn2.setAttribute("disabled", "true")
+            winner.innerText = "Player 1 won"
+        }
+    }  else (target  === "p1score"){
+        p2score = Number(scoreBoardP2.innerText);
+        p2score++;
+        scoreBoardP1.innerText = p2score;
+        if (p2score === targetNum){
+            btn1.setAttribute("disabled", "true")
+            btn2.setAttribute("disabled", "true")
+            winner.innerText = "Player 1 won"
+        }
+    }
+
 }
